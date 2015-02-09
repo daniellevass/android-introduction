7606635a94b673ab6ee2ca82568ccae743e09764
danielle vass <danielle@de-velopment.com>
2015-02-09 18:02:04 +0000

    list view locations screen

--- Flickr-viewer/app/src/main/java/danielle/dev/com/flickrviewer/LocationsActivity.java
+++ Flickr-viewer/app/src/main/java/danielle/dev/com/flickrviewer/LocationsActivity.java
@@ -4,36 +4,109 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class LocationsActivity extends ActionBarActivity {
+
+    private ListView listLocations;
+    private List<String> cities;
+    private List<String> countries;
+    private List<Integer> flags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations);
+
+        listLocations = (ListView)findViewById(R.id.listLocations);
+
+        cities = new ArrayList<String>();
+
+        cities.add("Hong Kong");
+        cities.add("London");
+        cities.add("San Francisco");
+        cities.add("Madrid");
+        cities.add("Sydney");
+        cities.add("New York");
+        cities.add("Bath");
+        cities.add("Cardiff");
+        cities.add("Dallas");
+
+
+        countries = new ArrayList<String>();
+
+        countries.add("China");
+        countries.add("United Kingdom");
+        countries.add("USA");
+        countries.add("Spain");
+        countries.add("Australia");
+        countries.add("USA");
+        countries.add("United Kingdon");
+        countries.add("Wales");
+        countries.add("USA");
+
+        flags = new ArrayList<Integer>();
+
+        flags.add(R.drawable.hongkong);
+        flags.add(R.drawable.unitedkingdom);
+        flags.add(R.drawable.unitedstates);
+        flags.add(R.drawable.spain);
+        flags.add(R.drawable.australia);
+        flags.add(R.drawable.unitedstates);
+        flags.add(R.drawable.unitedkingdom);
+        flags.add(R.drawable.wales);
+        flags.add(R.drawable.unitedstates);
+
+
+
+        LocationAdapter adapter = new LocationAdapter(cities);
+
+        listLocations.setAdapter(adapter);
+
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_locations, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class LocationAdapter extends ArrayAdapter<String> {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public LocationAdapter(List<String> items) {
+            super(LocationsActivity.this, 0, items);
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(
+                        R.layout.row_location, null);
+            }
+            ImageView imgIcon = (ImageView)convertView.findViewById(R.id.imgIcon);
+            TextView lblCity = (TextView)convertView.findViewById(R.id.lblCity);
+            TextView lblCountry = (TextView)convertView.findViewById(R.id.lblCountry);
+
+            String city = cities.get(position);
+            String country = countries.get(position);
+            int flag = flags.get(position);
+
+
+            imgIcon.setImageResource(flag);
+            lblCountry.setText(country);
+            lblCity.setText(city);
+
+            return convertView;
+        }// end get view
+
+    }// end adapter class
+
+
+
 }
