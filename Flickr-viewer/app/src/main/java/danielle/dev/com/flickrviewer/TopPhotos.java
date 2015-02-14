7795b5e863296370303f8c8999721f2ccf600d1d
danielle vass <danielle@de-velopment.com>
2015-02-14 23:45:15 +0000

    Add top locations activity

--- Flickr-viewer/app/src/main/java/danielle/dev/com/flickrviewer/TopPhotos.java
+++ Flickr-viewer/app/src/main/java/danielle/dev/com/flickrviewer/TopPhotos.java
@@ -5,20 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class SearchActivity extends ActionBarActivity {
+public class TopPhotos extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_top_photos);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
+        getMenuInflater().inflate(R.menu.menu_top_photos, menu);
         return true;
     }
 
